2488bdede8587cdee29c2dd7664d2746dd7d0274
li.jiang@shuruitech.com <li.jiang@shurutiech.com>
2021-01-08 19:20:27 +0800

    [UPDATE] add new topic

--- lc-code/src/test/com/util/TreeBuilderUtilTest.java
+++ lc-code/src/test/com/util/TreeBuilderUtilTest.java
@@ -1,15 +1,7 @@
 package com.util;
 
 import com.pojo.TreeNode;
-import org.junit.Test;
 
 
 class TreeBuilderUtilTest {
-
-    @Test
-    void buildBinaryTree() {
-        Integer[] nums = new Integer[]{1,2,3,null,null,4,5};
-        TreeNode treeNode = TreeBuilderUtil.buildBinaryTree(nums,0);
-        System.out.println(treeNode);
-    }
 }
